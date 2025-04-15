a0562db87d84efcee3a9eb4e37b3cb95270070ef
SkublicsImre <imre.skublics@gmail.com>
2025-04-15 13:48:57 +0200

    Add blank UI and display window

--- WinFormsApp1/WinFormsApp1/UI.Designer.cs
+++ WinFormsApp1/WinFormsApp1/UI.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinFormsApp1
 {
-    partial class Form1
+    partial class UI
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1986, 1237);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
--- WinFormsApp1/WinFormsApp1/UI.cs
+++ WinFormsApp1/WinFormsApp1/UI.cs
@@ -1,8 +1,8 @@
 namespace WinFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class UI : Form
     {
-        public Form1()
+        public UI()
         {
             InitializeComponent();
         }
